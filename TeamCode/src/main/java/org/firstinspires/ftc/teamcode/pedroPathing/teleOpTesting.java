7acbc3f6acd2068e65a47b1c5b6c5f17f1a949c5
BrandonTran9 <144183256+BrandonTran9@users.noreply.github.com>
2025-10-17 02:56:40 -0700

    new

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/teleOpTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/teleOpTesting.java
@@ -136,17 +136,16 @@ public class teleOpTesting extends LinearOpMode {
             }
 
 
-//            if (counter_c % 2 == 0) {
-//                spinner.setPower(0.1);
-//                try {
-//                    Thread.sleep(1000); // wait 1 second
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                System.out.println("1 second later...");
-//            } else {
-//                spinner.setPower(0); // ✅ Correct motor to stop
-//            }
+           if (counter_c % 2 == 0) {
+                spinner.setPower(0.1);
+                try {
+                    Thread.sleep(1000); // wait 1 second
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+           } else {
+                spinner.setPower(0); // ✅ Correct motor to stop
+            }
 
 
 
